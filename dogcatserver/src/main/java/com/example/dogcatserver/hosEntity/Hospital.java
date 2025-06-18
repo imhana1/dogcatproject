a90be20290b8e9e8144f9955b2be4e8766c03e2a
mid210 <midoriya2109@gmail.com>
2025-06-18 16:47:48 +0900

    hospital entity 추가

---
+++ dogcatserver/src/main/java/com/example/dogcatserver/hosEntity/Hospital.java
@@ -0,0 +1,28 @@
+package com.example.dogcatserver.hosEntity;
+
+import lombok.*;
+
+import java.time.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Hospital {
+    private String Husername;
+    private String director;
+    private String hospital;
+    private String tel;
+    private String reptel;
+    private String address;
+    private boolean choice;
+    private String profile;
+    private Double loaction;
+    private Double longitude;
+    private LocalDateTime openTime;
+    private LocalDateTime closeTime;
+    private String Dprofile;
+    private LocalDateTime birthDay;
+    private String email;
+    private String Introduction;
+}
