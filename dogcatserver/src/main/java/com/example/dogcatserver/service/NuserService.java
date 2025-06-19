64c22d417a7bdfc957d95e116de68a97f0084915
imhana1 <ksui3047@gmail.com>
2025-06-19 14:00:02 +0900

    pull하기 전 커밋;

--- dogcatserver/src/main/java/com/example/dogcatserver/service/NuserService.java
+++ dogcatserver/src/main/java/com/example/dogcatserver/service/NuserService.java
@@ -4,12 +4,15 @@ import com.example.dogcatserver.dao.NuserDao;
 import com.example.dogcatserver.dto.NuserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class NuserService {
     @Autowired
     private NuserDao nuserDao;
     @Autowired
     private PasswordEncoder encoder;
 
-    public boolean checkNid(NuserDto.NidCheck dto) {return !nuserDao.existsBynId(dto.getnId());}
+    public boolean checkNid(NuserDto.NidCheck dto) {return !nuserDao.existsBynId(dto.getNId());
+    }
 }
