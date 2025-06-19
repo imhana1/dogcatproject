ab2d55f1a3ea27b8b2b2ed662380f99fdb299907
Ha Yeon <chaddol31000@naver.com>
2025-06-19 16:52:59 +0900

    250619 pay 수정 + status 추가 + sql 수정

--- dogcatserver/src/main/java/com/example/dogcatserver/entity/Pay.java
+++ dogcatserver/src/main/java/com/example/dogcatserver/entity/Pay.java
@@ -10,10 +10,16 @@ import java.time.*;
 @Builder
 public class Pay {
   // rno, h_username, nusername 외래키
+  // p_name : 결제명
+  // pay_seq : 시퀀스
   private int rno;
   private String h_username;
   private String nusername;
+  private String orderId;
+  private String paymentKey;
   private String p_username;
+  private String p_name;
   private LocalDateTime p_time;
   private int p_price;
+  private PaymentStatus p_status;
 }
