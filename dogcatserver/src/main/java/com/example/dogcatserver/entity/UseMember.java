ec762252e6dfb605bb4458cd46ed20ed7408f09b
mid210 <midoriya2109@gmail.com>
2025-06-19 15:59:54 +0900

    hospital엔티티 컬럼 변경

--- dogcatserver/src/main/java/com/example/dogcatserver/entity/UseMember.java
+++ dogcatserver/src/main/java/com/example/dogcatserver/entity/UseMember.java
@@ -2,6 +2,8 @@ package com.example.dogcatserver.entity;
 
 import lombok.*;
 
+import java.time.*;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
@@ -10,4 +12,9 @@ public class UseMember {
     private String username;
     private String password;
     private Role role;
+    private String status;
+    private String e_code;
+    private int count;
+    private LocalDateTime sign_dt;
+    private int isLocked;
 }
