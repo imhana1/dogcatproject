cae7f497c3b070f2b8c1b330c5be3a3cbb365160
imhana1 <ksui3047@gmail.com>
2025-06-19 15:01:40 +0900

    일반 회원 회원가입

--- dogcatserver/src/main/java/com/example/dogcatserver/hosDao/HospitalDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.dogcatserver.hosDao;
-
-import org.apache.ibatis.annotations.*;
-
-@Mapper
-public interface HospitalDao {
-    @Select("Select count(*) from bmember where username=#{username}")
-    boolean existsByUsername(String username);
-}
