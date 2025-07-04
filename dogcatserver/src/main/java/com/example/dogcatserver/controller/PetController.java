ca3ec4df3346998661cfb5749883367bb2bec408
wvriinylont <sera7418@naver.com>
2025-07-04 16:57:21 +0900

    0704 진료결과 하는중 아직 백 연결 안됨, 백 import

--- dogcatserver/src/main/java/com/example/dogcatserver/controller/PetController.java
+++ dogcatserver/src/main/java/com/example/dogcatserver/controller/PetController.java
@@ -3,6 +3,7 @@ package com.example.dogcatserver.controller;
 import com.example.dogcatserver.dto.PetDto;
 import com.example.dogcatserver.entity.Pet;
 import com.example.dogcatserver.service.PetService;
+import com.example.dogcatserver.util.ProfileUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
