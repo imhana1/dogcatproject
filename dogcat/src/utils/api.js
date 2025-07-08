aa4797cb751464dc63674ac1740b286a6d4e101c
imhana1 <ksui3047@gmail.com>
2025-07-08 16:51:46 +0900

    펫 정보 프로필 안읽어옴, 펫 삭제 만드는중

--- dogcat/src/utils/api.js
+++ dogcat/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-
-
 const api = axios.create({baseURL:'http://localhost:8080', withCredentials: true});
+
+export const deletePet = (pno) => api.delete(`api/nuser-petchange?pno=${pno}`)
 
 export default api;
