9b6254c6b13b681ba8b5d726af143d141427835f
전효진 <wjsgywls17@gmail.com>
2025-07-11 09:03:56 +0900

    0711.fri: nav에 마이페이지 추가, app.js에 토스 주석 걸어둠

--- dogcat/src/fragments/noticeQna/NavNoticeQna.js
+++ dogcat/src/fragments/noticeQna/NavNoticeQna.js
@@ -7,13 +7,24 @@ import {findQnaQuestionByQnoWithAnswer} from "../../utils/qnaApi";
 
 
 function NavNoticeQna({ activeTab }) {
+  const {username, role} = useAuthStore();
   return (
     <div className={styles.ntcQnaWrapper}>
       <nav>
+        {role === 'ADMIN' && (
+          <>
         <a type='button' href='/n-members' className={activeTab === 'nMemberManage' ? 'btn btn-secondary' : 'btn btn-dark'} > 일반 회원 관리</a>
         <a type='button' href='/h-members' className={activeTab === 'hMemberManage' ? 'btn btn-secondary' : 'btn btn-dark'}>병원 회원 관리</a>
+          </>
+      )}
         <a type='button' href='/notices' className={activeTab === 'notice' ? 'btn btn-secondary' : 'btn btn-dark'}>공지사항</a>
         <a type='button' href='/qna' className={activeTab === 'qna' ? 'btn btn-secondary' : 'btn btn-dark'}>1:1 문의</a>
+        {username && role === 'USER' && (
+          <a type='button' href='/nuser-mypage' className={activeTab === 'nuser-mypage' ? 'btn btn-secondary' : 'btn btn-dark'}>마이페이지</a>
+        )}
+        {username && role === 'HOSPITAL' && (
+          <a type='button' href='/hospital-mypage' className={activeTab === 'hospital-mypage' ? 'btn btn-secondary' : 'btn btn-dark'}>마이페이지</a>
+        )}
       </nav>
     </div >
   )
